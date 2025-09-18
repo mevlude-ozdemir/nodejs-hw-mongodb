f9317fb314e8b0a84a5d1eb2562d64ad765c0944
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 20:07:20 +0300

    Step 2- 3

---
+++ src/server.js
@@ -0,0 +1,75 @@
+// src/server.js
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+// import { getAllStudents, getStudentById } from './services/students.js';
+//4. Sunucunun, PORT ortam değişkeni aracılığıyla belirtilen veya belirtilmemişse 3000 numaralı portta başlatılması
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  //1.express() çağrısıyla sunucunun oluşturulması
+  const app = express();
+  //2.cors ve pino logger'ının ayarlanması
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+//   app.get('/students', async (req, res) => {
+//     const students = await getAllStudents();
+
+//     res.status(200).json({
+//       data: students,
+//     });
+//   });
+
+//   app.get('/students/:studentId', async (req, res) => {
+//     const { studentId } = req.params;
+//     const student = await getStudentById(studentId);
+
+//     // Öğrenci bulunamazsa yanıt
+//     if (!student) {
+//       res.status(404).json({
+//         message: 'Öğrenci bulunamadı',
+//       });
+//       return;
+//     }
+
+//     // Öğrenci bulunursa yanıt
+//     res.status(200).json({
+//       data: student,
+//     });
+//   });
+
+   //3.Mevcut olmayan rotalar için 404 hata durumu ve uygun mesaj döndürülmesi.
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+ 
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+//5. Sunucu başarıyla başlatıldığında konsola “Server is running on port {PORT}” mesajının yazdırılması; burada {PORT} sizin port numaranızdır
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
