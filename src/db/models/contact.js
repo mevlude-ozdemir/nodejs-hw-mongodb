e46584c35c9b0ca6ae13bf306a7f0ab991cfddbb
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 20:18:59 +0300

    Step 4

---
+++ src/db/models/contact.js
@@ -0,0 +1,40 @@
+// src/db/models/contact.js
+
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    //   required: true,
+      
+    },
+     isFavourite : {
+      type: Boolean,
+    //   required: true,
+      default: false,
+    },
+    contactType : {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+   
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+
+export const ContactsCollection = model('contacts', contactsSchema);
