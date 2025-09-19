c0b0479d2592af043729176e8fb250c5c4c216a2
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-19 21:58:43 +0300

    step 6

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -7,6 +7,8 @@ import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema} from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
+import { logoutUserController } from '../controllers/auth.js';
+import { refreshUserSessionController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -20,5 +22,7 @@ router.post(
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
+router.post('/logout', ctrlWrapper(logoutUserController));
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
