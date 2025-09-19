87ef70adbc92e33fdfed228cb55bc390820a3080
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-19 21:48:20 +0300

    step 6

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,6 +5,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { loginUserSchema} from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -12,6 +14,11 @@ router.post(
   '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
 );
 
 export default router;
