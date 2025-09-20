aafadb0fd9968c6f73cfd3ad29d774e5110ac10d
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-20 04:30:10 +0300

    reset-password test edildi. çalışıyor

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,16 +1,15 @@
 // src/utils/sendMail.js
 
 import nodemailer from 'nodemailer';
-
-import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
 const transporter = nodemailer.createTransport({
-  host: env(SMTP.SMTP_HOST),
-  port: Number(env(SMTP.SMTP_PORT)),
+  host: env('SMTP_HOST'),
+  port: Number(env('SMTP_PORT')),
+  secure: false,
   auth: {
-    user: env(SMTP.SMTP_USER),
-    pass: env(SMTP.SMTP_PASSWORD),
+    user: env('SMTP_USER'),
+    pass: env('SMTP_PASSWORD'),
   },
 });
 
